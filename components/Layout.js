7d9cb2f35dc8834ab6a9da0244680d59e216edd4
Iris Ibekwe <iris.ibekwe@civicactions.com>
2022-11-22 22:38:09 -0500

    Add variable google fonts.

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,17 @@
 import Header from './Header'
+// Fonts
+import { Urbanist } from '@next/font/google'
+
+const urbanist = Urbanist({ subsets: ['latin'] })
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className={urbanist.className}>
       <Header />
       <div className='layout'>
         {children}
       </div>
-    </>
+    </div>
   )
 
 }
