ae79a5ee7914fbfbd1acd87f961dd22901af73f0
Iris Ibekwe <iris.ibekwe@civicactions.com>
2022-12-06 05:43:01 -0500

    Add sass and tailwind.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    // Load sass files by @import filename.scss instead of typing the full paths.
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   experimental: {
     swcPlugins: [
       [
