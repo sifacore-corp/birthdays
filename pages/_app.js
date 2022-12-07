e84c556f066287ad2981e6ddc09410b9b2ef8406
Iris Ibekwe <iris.ibekwe@civicactions.com>
2022-12-07 11:52:37 -0500

    Switch entry point from typescript to JS.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/globals.scss'
 import { SessionProvider, useSession } from 'next-auth/react';
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       {
